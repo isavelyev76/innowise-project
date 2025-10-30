0d85cc70a9f716a4a342bf5f3fe742aec4563db8
isavelyev76 <etodlajava@gmail.com>
2025-10-30 22:15:32 +0300

    project init

---
+++ backend/user-service/src/test/java/ru/isavelev76/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isavelev76.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
